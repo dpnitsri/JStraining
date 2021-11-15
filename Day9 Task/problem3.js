a454d2892a684d61999483dc0175f63bca7cc8a9
dpnitsri <40091213+dpnitsri@users.noreply.github.com>
2021-11-15 15:37:12 +0530

    Create problem3.js

---
+++ Day9 Task/problem3.js
@@ -0,0 +1,10 @@
+/*3. Take another paramter as salary and keep it above 500000 in numbers in json and print it upto one decimal place only with unit as Lac*/
+
+import employeesData from './employees.json';
+
+
+for(index in employeesData){
+    employeesData[index].salary= ((500000*(Math.random+1))/100000).toFixed(1) + 'Lac';
+}
+
+console.log(employeesDate);
