69250a02510f5b890825870d7c1c2d996f91fd07
dpnitsri <40091213+dpnitsri@users.noreply.github.com>
2021-11-15 15:23:58 +0530

    Create problem1.js

---
+++ Day9 Task/problem1.js
@@ -0,0 +1,9 @@
+/*1. Add the list of employees in a json file and import it in below file to use it. Add unique ids for each employee.*/
+
+import employeesData from './employees.json';
+
+for(index in employeesData)
+    employeesData[index].id= index;
+
+console.log(employeesData);
+
