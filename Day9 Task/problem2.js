7aec4dcad48e31e380b8d0341ceec92fafe0bb1b
dpnitsri <40091213+dpnitsri@users.noreply.github.com>
2021-11-15 15:27:08 +0530

    Create problem2.js

---
+++ Day9 Task/problem2.js
@@ -0,0 +1,6 @@
+/*2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.*/
+
+import { searchName } from './searchName.js';
+
+const employeesNameUpdate = searchName(employeesData);
+console.log(employeesNameUpdate);
