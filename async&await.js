9a850ceab96ae3a34e708406c364288f7232d7ef
dpnitsri <40091213+dpnitsri@users.noreply.github.com>
2021-11-17 14:13:24 +0530

    Create async&await.js

---
+++ async&await.js
@@ -0,0 +1,28 @@
+/* - Create a function that returns array of employees from a random time from 1 to 2 seconds using async await.*/
+
+const employees = [{
+    "name":"Deepak",
+    "age": 25
+},
+{
+    "name":"Abhishek",
+    "age":22
+}];
+
+const employeesList = () =>{
+     return new Promise((resolve)=>
+     {
+        setTimeout(function(){
+        const response =  employees;
+        resolve(response);
+        },1000 + Math.random()*1000);
+     });
+}
+
+
+const employeesDetails = async () =>{
+    const resolvedList = await employeesList();
+    console.log(resolvedList);
+}
+
+employeesDetails();
